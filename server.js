5b5a563c100117732edada203b941d3301a174dc
MatejNavara <matej.navara@gmail.com>
2017-05-01 22:14:12 +0100

    Working api calls between angular front-end and MongoDB

--- server.js
+++ server.js
@@ -77,6 +77,11 @@
         });
     });
 
+    // application -------------------------------------------------------------
+    app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+
     // listen (start app with node server.js) ======================================
     app.listen(8080);
     console.log("App listening on port 8080");
